d119101f16d39d8b46556e68341c2d1b59991332
Ray-Shakex <bshakex@gmail.com>
2021-01-16 11:45:36 +0200

    initialised a bootstrap scrollbar repository

---
+++ main.js
@@ -0,0 +1,23 @@
+$(function(){
+    $("#content-1").mCustomScrollbar({
+        theme: "rounded-dots-dark"
+    });
+
+    $("#content-2").mCustomScrollbar({
+        theme: "rounded-dark"
+    });
+
+    $("#content-3").mCustomScrollbar({
+        theme: "inset-3-dark"
+    });
+
+    $("#content-4").mCustomScrollbar({
+        theme: "3d-dark"
+    });
+
+    $("#content-5").mCustomScrollbar({
+        theme: "dark-thin"
+    });
+
+
+});
